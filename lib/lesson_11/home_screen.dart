d741aa49bc03b523f8fde6a45e79da0f07f3a6f8
nazarenkodima <dmytro.nazarenko@hey.com>
2026-04-18 15:44:20 +0300

    feat: add home screen with subtasks placeholders

---
+++ lib/lesson_11/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const _icons = [
+    Icons.looks_one,
+    Icons.looks_two,
+    Icons.looks_3,
+    Icons.looks_4,
+    Icons.looks_5,
+  ];
+
+  static const _listLenght = 5;
+
+  final List<Widget> _screens = List.generate(
+    _listLenght,
+    (index) => Center(child: Text('Subtask ${index + 1}')),
+  );
+
+  final List<BottomNavigationBarItem> _items = List.generate(
+    _listLenght,
+    (index) => BottomNavigationBarItem(
+      icon: Icon(_icons[index]),
+      label: '${index + 1}',
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Subtask ${_selectedIndex + 1}',
+            style: TextStyle(fontWeight: .bold),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: _items,
+      ),
+    );
+  }
+}
