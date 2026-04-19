9add14a569d6d2f732bb250898b0809392b609a9
nazarenkodima <dmytro.nazarenko@hey.com>
2026-04-19 09:01:22 +0300

    fix(subtask 4): align column center

--- lib/lesson_11/homework_11_subtask4.dart
+++ lib/lesson_11/homework_11_subtask4.dart
@@ -45,24 +45,27 @@ class SubtaskFour extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(47, 0, 46, 0),
         child: SizedBox(
           height: 659,
-          child: Column(
-            spacing: 8,
-            children: [
-              Expanded(
-                child: _card(
-                  color: const Color(0xFF096EEA),
-                  alignment: Alignment.topLeft,
+
+          child: Center(
+            child: Column(
+              spacing: 8,
+              children: [
+                Expanded(
+                  child: _card(
+                    color: const Color(0xFF096EEA),
+                    alignment: Alignment.topLeft,
+                  ),
                 ),
-              ),
-              _card(
-                color: const Color(0xFF08E900),
-                alignment: Alignment.center,
-              ),
-              _card(
-                color: const Color(0xFFEA090C),
-                alignment: Alignment.bottomRight,
-              ),
-            ],
+                _card(
+                  color: const Color(0xFF08E900),
+                  alignment: Alignment.center,
+                ),
+                _card(
+                  color: const Color(0xFFEA090C),
+                  alignment: Alignment.bottomRight,
+                ),
+              ],
+            ),
           ),
         ),
       ),
