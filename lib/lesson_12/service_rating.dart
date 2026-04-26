c1d90ab83e8ff1023d8301ef1edd17fae4ccb78b
nazarenkodima <dmytro.nazarenko@hey.com>
2026-04-26 16:17:46 +0300

    feat(service rating): fix color

--- lib/lesson_12/service_rating.dart
+++ lib/lesson_12/service_rating.dart
@@ -18,7 +18,7 @@ class ServiceRating extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       decoration: BoxDecoration(
-        color: const Color(0xFFEEF2FC),
+        color: const Color(0xFFF6F8FD),
         borderRadius: BorderRadius.circular(12),
       ),
       child: Row(
