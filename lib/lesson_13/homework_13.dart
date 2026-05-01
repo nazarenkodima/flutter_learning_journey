d3a7c15a30472090b089214281148d24493af3cd
nazarenkodima <dmytro.nazarenko@hey.com>
2026-05-01 12:44:46 +0300

    complete task 8

--- lib/lesson_13/homework_13.dart
+++ lib/lesson_13/homework_13.dart
@@ -239,7 +239,7 @@ class TrainingExample8 extends StatelessWidget {
     return Column(
       children: [
         Expanded(child: Container(color: Colors.red)),
-        Expanded(child: Container(color: Colors.green)),
+        Expanded(flex: 2, child: Container(color: Colors.green)),
         Expanded(child: Container(color: Colors.blue)),
       ],
     );
