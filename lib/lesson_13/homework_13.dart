1deb75ce4ac63e565160097659c780773720fb4d
nazarenkodima <dmytro.nazarenko@hey.com>
2026-05-01 12:42:28 +0300

    complete task 7

--- lib/lesson_13/homework_13.dart
+++ lib/lesson_13/homework_13.dart
@@ -217,9 +217,11 @@ class TrainingExample7 extends StatelessWidget {
     return Column(
       children: [
         Container(color: Colors.orange, height: 100, width: 100),
-        ListView.builder(
-          itemCount: 50,
-          itemBuilder: (context, index) => Text('Item $index'),
+        Flexible(
+          child: ListView.builder(
+            itemCount: 50,
+            itemBuilder: (context, index) => Text('Item $index'),
+          ),
         ),
       ],
     );
