26d8f454cb66cb4926a59c48e192307b06bb6829
nazarenkodima <dmytro.nazarenko@hey.com>
2026-05-05 10:00:14 +0300

    feat: add RouteNames

--- lib/lesson_16/homework/main.dart
+++ lib/lesson_16/homework/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_learning_journey/features/screens/home_screen.dart';
+import 'package:flutter_learning_journey/router/routes_names.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
   routes: [
-    GoRoute(path: '/', name: 'home', builder: (context, state) => HomeScreen()),
+    GoRoute(
+      path: '/',
+      name: RouteNames.home,
+      builder: (context, state) => HomeScreen(),
+    ),
   ],
 );
